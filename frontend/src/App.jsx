05aade136bc408d0ce6972e4647029a0c3b1909c
Hemanth Chakravarthy Kancharla <127734226+hemanth-chakravarthy@users.noreply.github.com>
2024-09-25 11:27:53 +0530

    Landing_Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,42 @@
-import React from "react"
-import {Routes, Route} from 'react-router-dom'
-import Home from "./pages/Home"
-import Admin from "./pages/Admin"
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Services from './pages/Services';
+import SignUp from './pages/SignUp';
+import LogIn from './pages/LogIn'
+import Destination from './pages/Destination';
+import ScrollToTop from './components/ScrollToTop';
+import Admin from './pages/Admin';
 import DeleteEntity from './pages/DeleteEntity';
-import ShowEntity from './pages/ShowEntity'
-import UpdateEntity from "./pages/UpdateEntity";
+import ShowEntity from './pages/ShowEntity';
+import UpdateEntity from './pages/UpdateEntity';
+import './App.css';
 
-const App = () =>{
-  return(
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/admin" element={<Admin />}/>
-      <Route path="/admin/:entity/delete/:id" element={<DeleteEntity />} />
-      <Route path="/admin/:entityType/:id" element={<ShowEntity/>}/>
-      <Route path="/admin/:entityType/edit/:id" element={<UpdateEntity/>}/>
-    </Routes>
-  )
-}
+const App = () => {
+  return (
+    <div className="App">
+      {/* Add Navbar to all routes */}
+      <Navbar />
+      {/* Scroll to top on route change */}
+      <ScrollToTop>
+        <Routes>
+          {/* Routes from Main */}
+          <Route path="/" exact element={<Home />} />
+          <Route path="/services" exact element={<Services />} />
+          <Route path="/sign-up" exact element={<SignUp />} />
+          <Route path="/login" exact element={<LogIn />}/>
+          <Route path="/services/activity" element={<Destination />} />
 
-export default App
+          {/* Routes from Admin section */}
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/admin/:entity/delete/:id" element={<DeleteEntity />} />
+          <Route path="/admin/:entityType/:id" element={<ShowEntity />} />
+          <Route path="/admin/:entityType/edit/:id" element={<UpdateEntity />} />
+        </Routes>
+      </ScrollToTop>
+    </div>
+  );
+};
+
+export default App;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import '../App.css';
+import Cards from '../components/Cards';
+import Footer from '../components/Footer';
+import HeroSection from '../components/HeroSection';
 
-const Home = () => {
+function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <HeroSection />
+      <Cards />
+      <Footer />
+    </>
+  );
 }
 
-export default Home
+export default Home;
