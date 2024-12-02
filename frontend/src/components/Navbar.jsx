99b2581e50a967f54423c2d35a8fc5dbe1994850
Sameera <kusalasameera@gmail.com>
2024-12-02 12:53:55 +0530

    Create Package and home page frontend layout done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,20 +1,61 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-        <nav>
-            <img src="/images/echo_voyage_logo.png" className="nav--icon" />
-            <div className="nav-elements">
-            <Link to={`/home`} className="nav--element--2">Home</Link>
-            <Link to={`/search`} className="nav--element--2">Search</Link>
-            <Link to={`/custProfilePage`} className="nav--element--2">Profile Page</Link>
-            <Link to={`/customerWishlist`} className="nav--element--1">Wishlist</Link>
-            <Link to={`/customerGuide`} className="nav--element--2">Guides</Link>
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">EchoVoyages</a>
+      </div>
+      <div className="flex-none gap-2">
+        <div className="flex space-x-4">
+          <Link to={`/home`} className="btn btn-ghost">
+            Home
+          </Link>
+          <Link to={`/search`} className="btn btn-ghost">
+            Search
+          </Link>
+          <Link to={`/custProfilePage`} className="btn btn-ghost">
+            Profile Page
+          </Link>
+          <Link to={`/customerWishlist`} className="btn btn-ghost">
+            Wishlist
+          </Link>
+          <Link to={`/customerGuide`} className="btn btn-ghost">
+            Guides
+          </Link>
+        </div>
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
             </div>
-        </nav>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a className="justify-between">Profile</a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
-}
-export default Navbar
+  );
+};
+
+export default Navbar;
--- frontend/src/pages/AgentHomePage.jsx
+++ frontend/src/pages/AgentHomePage.jsx
@@ -46,19 +46,24 @@ const AgentHomePage = () => {
 
   return (
     <div>
-      <nav className="navbarr">
-        <ul className="nav-linkss">
-          <li>
-            <Link to="/AgentHome">Home Page</Link>
-          </li>
-          <li>
-            <Link to="/createPackage">Create Package</Link>
-          </li>
-          <li>
-            <Link to="/AgentProfilePage">Profile Page</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">EchoVoyages</a>
+        </div>
+        <div className="flex-none gap-2">
+          <div className="flex space-x-4">
+            <Link to="/AgentHome" className="btn btn-ghost">
+              Home Page
+            </Link>
+            <Link to="/createPackage" className="btn btn-ghost">
+              Create Package
+            </Link>
+            <Link to="/AgentProfilePage" className="btn btn-ghost">
+              Profile Page
+            </Link>
+          </div>
+        </div>
+      </div>
       <h1>Listed Packages</h1>
       {bookedPackages.length > 0 ? (
         <ul>
