91c8c4325ce440f6babe994ad40bd8c208db61b9
S-a-m-93 <kusalasameera.g22@iiits.in>
2025-05-05 13:35:54 +0530

    [fix] meta env

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,4 @@
 /** @format */
-import dotenv from "dotenv";
-dotenv.config();
-
-const apiUrl = process.env.VITE_API_URL || "http://localhost:5000";
+const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 export default apiUrl;
