77373ca5fef8a4a9456d2dcb58fb03a0790c21be
MithileshIruvuri02 <139386068+MithileshIruvuri02@users.noreply.github.com>
2025-05-06 11:03:15 +0530

    Fixed swagger

--- backend/swaggerSetup.js
+++ backend/swaggerSetup.js
@@ -21,7 +21,7 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: 'http://localhost:5000',
+        url: 'https://echovoyages.onrender.com',
         description: 'Development server'
       }
     ]
